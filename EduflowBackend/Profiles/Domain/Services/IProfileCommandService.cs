398c1da3456a4798b0c567e30b4de1e9bb221210
RnArauj0 <u202113612@upc.edu.pe>
2025-06-17 21:10:46 -0500

    feat: services to IProfileCommandService.cs and IProfileQueryService.cs are created.

---
+++ EduflowBackend/Profiles/Domain/Services/IProfileCommandService.cs
@@ -0,0 +1,9 @@
+﻿using EduflowBackend.Profiles.Domain.Model.Aggregates;
+using EduflowBackend.Profiles.Domain.Model.Commands;
+
+namespace EduflowBackend.Profiles.Domain.Services;
+
+public interface IProfileCommandService
+{
+    Task<Profile?> Handle(CreateProfilesCommand command);
+}
---
+++ EduflowBackend/Profiles/Domain/Services/IProfileQueryService.cs
@@ -0,0 +1,30 @@
+﻿using EduflowBackend.Profiles.Domain.Model.Aggregates;
+using EduflowBackend.Profiles.Domain.Model.Queries;
+
+namespace EduflowBackend.Profiles.Domain.Services;
+
+/// <summary>
+/// Profile query service interface
+/// </summary>
+public interface IProfileQueryService
+{
+    /// <summary>
+    /// Handle retrieving all profiles
+    /// </summary>
+    Task<IEnumerable<Profile>> Handle(ListAllProfilesQuery query);
+
+    /// <summary>
+    /// Handle retrieving a profile by email
+    /// </summary>
+    Task<Profile?> Handle(GetProfileByEmailQuery query);
+
+    /// <summary>
+    /// Handle retrieving a profile by ID
+    /// </summary>
+    Task<Profile?> Handle(GetProfileByIdQuery query);
+
+    /// <summary>
+    /// Handle retrieving profiles by user type
+    /// </summary>
+    Task<IEnumerable<Profile>> Handle(ListProfilesByUserTypeQuery query);
+}
