3fcda545c6d0df2087e49631fc5eb28fb9a1e890
RnArauj0 <u202113612@upc.edu.pe>
2025-06-18 23:05:35 -0500

    feat: update AssemblyInformationalVersion in AssemblyInfo.cs

--- EduflowBackend/obj/Debug/net9.0/EduflowBackend.AssemblyInfo.cs
+++ EduflowBackend/obj/Debug/net9.0/EduflowBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduflowBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd3f30df4ddbb0866afb5ccdb9ecfc369abd0d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1945ef3a0ddb9f5cc157685142106591cd644607")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduflowBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduflowBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
