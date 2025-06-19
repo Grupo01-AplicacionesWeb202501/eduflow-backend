3e0d0baf75a6bbd6cfcb623f649705df1a73e4a2
RnArauj0 <u202113612@upc.edu.pe>
2025-06-18 21:27:42 -0500

    feat: implement ProfileCommandService for handling profile creation commands

---
+++ EduflowBackend/Profiles/Application/Internal/CommandServices/ProfileCommandService.cs
@@ -0,0 +1,35 @@
+﻿using EduflowBackend.Profiles.Domain.Model.Aggregates;
+using EduflowBackend.Profiles.Domain.Model.Commands;
+using EduflowBackend.Profiles.Domain.Repositories;
+using EduflowBackend.Profiles.Domain.Services;
+using EduflowBackend.Shared.Domain.Repositories;
+
+namespace EduflowBackend.Profiles.Application.Internal.CommandServices;
+
+/// <summary>
+/// Profile command service
+/// </summary>
+/// <param name="profileRepository">Profile repository</param>
+/// <param name="unitOfWork">Unit of work</param>
+public class ProfileCommandService(
+    IProfileRepository profileRepository,
+    IUnitOfWork unitOfWork
+) : IProfileCommandService
+{
+    /// <inheritdoc />
+    public async Task<Profile?> Handle(CreateProfilesCommand command)
+    {
+        var profile = new Profile(command);
+        try
+        {
+            await profileRepository.AddAsync(profile);
+            await unitOfWork.CompleteAsync();
+            return profile;
+        }
+        catch (Exception e)
+        {
+            // Log error (optional: inject a logger)
+            return null;
+        }
+    }
+}
