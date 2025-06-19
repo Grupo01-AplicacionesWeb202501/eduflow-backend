5b23e9b43bc5685c26671e647d066a9c41056774
RnArauj0 <u202113612@upc.edu.pe>
2025-06-18 21:39:35 -0500

    feat: add CreateProfileResource for profile creation

---
+++ EduflowBackend/Profiles/Interfaces/REST/Resources/CreateProfileResource.cs
@@ -0,0 +1,18 @@
+﻿namespace EduflowBackend.Profiles.Interfaces.REST.Resources;
+
+/// <summary>
+/// Resource for creating a new profile (student or teacher)
+/// </summary>
+/// <param name="FullName">Full name of the user</param>
+/// <param name="Email">Email address</param>
+/// <param name="UserType">User type: "student" or "teacher"</param>
+/// <param name="Career">Career (for students)</param>
+/// <param name="CurrentCycle">Cycle (for students)</param>
+/// <param name="Subject">Subject (for teachers)</param>
+public record CreateProfileResource(
+    string FullName,
+    string Email,
+    string UserType,
+    string? Career = null,
+    int? CurrentCycle = null,
+    string? Subject = null);
