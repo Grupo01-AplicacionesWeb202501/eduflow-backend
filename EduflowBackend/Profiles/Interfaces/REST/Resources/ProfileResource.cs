f7e61d4ffc80e257d39c466de9b8542aac19ebe5
RnArauj0 <u202113612@upc.edu.pe>
2025-06-18 21:43:55 -0500

    feat: add CreateProfileCommandFromResourceAssembler and ProfileResource for profile creation

---
+++ EduflowBackend/Profiles/Interfaces/REST/Resources/ProfileResource.cs
@@ -0,0 +1,20 @@
+﻿namespace EduflowBackend.Profiles.Interfaces.REST.Resources;
+
+/// <summary>
+/// Profile resource for REST API responses
+/// </summary>
+/// <param name="Id">Unique identifier of the profile</param>
+/// <param name="FullName">Full name of the user</param>
+/// <param name="Email">Email address</param>
+/// <param name="UserType">Type of user: "student" or "teacher"</param>
+/// <param name="Career">Career (for students)</param>
+/// <param name="CurrentCycle">Cycle (for students)</param>
+/// <param name="Subject">Subject taught (for teachers)</param>
+public record ProfileResource(
+    int Id,
+    string FullName,
+    string Email,
+    string UserType,
+    string? Career = null,
+    int? CurrentCycle = null,
+    string? Subject = null);
